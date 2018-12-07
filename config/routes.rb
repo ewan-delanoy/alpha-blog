817bd89844879bebd3b2e894d29bd5ab6713947e
Ewan Delanoy <ewan.delanoy@zoho.com>
2018-12-07 13:53:45 +0100

    begin implementing login

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   # alternative version : post 'users', to: 'users#create'
 
   resources :articles
