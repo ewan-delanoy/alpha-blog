aa79fefd9463afd10b497221ef1b7280fef8f2ef
Ewan Delanoy <ewan.delanoy@zoho.com>
2018-12-07 09:26:10 +0100

    add association between user and articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,7 +9,9 @@ class ArticlesController < ApplicationController
   def edit
   end
   def create
+    debugger
     @article = Article.new(article_params)
+    # @article.user = User.first 
     if @article.save
       flash[:notice] = "Article was successfully created"
       redirect_to article_path(@article)
