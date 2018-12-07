4799dbeae694d4b8949d3a39233cfbc7649830c7
Ewan Delanoy <ewan.delanoy@zoho.com>
2018-12-07 09:48:59 +0100

    add user data to articles listing page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,9 +9,8 @@ class ArticlesController < ApplicationController
   def edit
   end
   def create
-    debugger
     @article = Article.new(article_params)
-    # @article.user = User.first 
+    @article.user = User.first 
     if @article.save
       flash[:notice] = "Article was successfully created"
       redirect_to article_path(@article)
