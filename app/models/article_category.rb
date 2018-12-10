5732c97e7d21022209c73654286b53ff75e3037b
Ewan Delanoy <ewan.delanoy@zoho.com>
2018-12-10 12:34:31 +0100

    Add article-category association

--- app/models/article_category.rb
+++ app/models/article_category.rb
@@ -1,7 +1,6 @@
-class Article < ApplicationRecord
+class ArticleCategory < ApplicationRecord
   belongs_to :user
-  has_many :article_categories
-  has_many :categories, through: :article_categories
+  belongs_to :category
   validates :title, presence: true, length: {minimum: 3, maximum: 50}
   validates :description, presence: true,  length: {minimum: 10, maximum: 100}
   validates :user_id, presence: true
