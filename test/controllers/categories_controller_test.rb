d511d08d78b22c5b8c8a2a23770daa30e28bcbf3
Ewan Delanoy <ewan.delanoy@zoho.com>
2018-12-10 12:05:32 +0100

    add admit requirement to categories creation

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,17 +3,26 @@ require 'test_helper'
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @category = Category.create(name: "sports")
+    @admin_user = User.create({username: "admin", email: "admin@example.com", password:"password", admin: true})
   end
   test "should get categories index" do
     get categories_path
     assert_response :success
   end
   test "should get new category action" do
+    sign_in_as(@admin_user,"password")
     get new_category_path
     assert_response :success
   end
   test "should get show category action" do
     get category_path(@category)
     assert_response :success
+  end
+
+  test "should redirect create when admin not logged in" do
+    assert_no_difference 'Category.count' do
+      post categories_path, params: {category:{name:"sports"}}
+    end
+    assert_redirected_to categories_path
   end
 end
