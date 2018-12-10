9b7de2582b686de7838eef49b4f54551c90cc9bc
Ewan Delanoy <ewan.delanoy@zoho.com>
2018-12-10 12:22:12 +0100

    Fix tests and update navbar with categories

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -3,8 +3,10 @@ require 'test_helper'
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
   def setup
     @category = Category.create(name: "sports")
+    @admin_user = User.create({username: "admin", email: "admin@example.com", password:"password", admin: true})
   end
   test "get new category form and create category" do
+    sign_in_as(@admin_user,"password")
     get new_category_path
     assert_response :success
     assert_template 'categories/new'
@@ -17,6 +19,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
   end
 
   test "invalid category submission results in failure" do
+    sign_in_as(@admin_user,"password")
     get new_category_path
     assert_response :success
     assert_template 'categories/new'
