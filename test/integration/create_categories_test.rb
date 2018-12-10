a84ddc9e076dbdcd7329186223f82479dd206266
Ewan Delanoy <ewan.delanoy@zoho.com>
2018-12-10 11:42:28 +0100

    add categories model and tests

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -15,4 +15,17 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     assert_template 'categories/index'
     assert_match 'sports', response.body
   end
+
+  test "invalid category submission results in failure" do
+    get new_category_path
+    assert_response :success
+    assert_template 'categories/new'
+    assert_no_difference 'Category.count' do
+      post categories_path, params: {category:{name:" "}}
+      follow_redirect!
+    end
+    assert_template 'categories/new'
+    assert_template 'div.panel-body'
+
+  end
 end
