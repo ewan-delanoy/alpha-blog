a9007639a855880a33587958a279e8c7904447c7
Ewan Delanoy <ewan.delanoy@zoho.com>
2018-12-11 06:24:30 +0100

    removed validations in article_catgeory model

--- app/models/article_category.rb
+++ app/models/article_category.rb
@@ -1,7 +1,4 @@
 class ArticleCategory < ApplicationRecord
   belongs_to :user
   belongs_to :category
-  validates :title, presence: true, length: {minimum: 3, maximum: 50}
-  validates :description, presence: true,  length: {minimum: 10, maximum: 100}
-  validates :user_id, presence: true
 end
