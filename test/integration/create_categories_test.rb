4c8fad9a89f69079d4bb4fa2f5cf0126009d5854
Ewan Delanoy <ewan.delanoy@zoho.com>
2018-12-14 11:34:21 +0100

    update all views with categories

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
   def setup
-    @category = Category.create(name: "sports")
+    @category = Category.create(name: "dance")
     @admin_user = User.create({username: "admin", email: "admin@example.com", password:"password", admin: true})
   end
   test "get new category form and create category" do
@@ -12,7 +12,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     assert_template 'categories/new'
     assert_difference 'Category.count', 1 do
       post categories_path, params: {category:{name:"sports"}}
-      follow_redirect!
+      # follow_redirect!
     end
     assert_template 'categories/index'
     assert_match 'sports', response.body
@@ -25,7 +25,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     assert_template 'categories/new'
     assert_no_difference 'Category.count' do
       post categories_path, params: {category:{name:" "}}
-      follow_redirect!
+      # follow_redirect!
     end
     assert_template 'categories/new'
     assert_template 'div.panel-body'
